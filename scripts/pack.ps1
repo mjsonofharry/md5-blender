f1d659cb4af718d5370d23a438d0b565f7ab750f
mjharrison <mjsonofharry@gmail.com>
2020-05-25 12:53:16 -0400

    trivial formatting changes

--- scripts/pack.ps1
+++ scripts/pack.ps1
@@ -5,11 +5,9 @@ $tmp_path = "$project_path\scripts\io_scene_md5model"
 If (Test-Path $tmp_path) {
     Remove-Item -Path $tmp_path -Recurse -Force
 }
-
 New-Item -Path $tmp_path -ItemType directory -Force
 New-Item -Path $tmp_path\plugin -ItemType directory -Force
 Copy-Item -Path $sources_path\*.py -Destination $tmp_path -Force
-
 Copy-Item -Path $sources_path\plugin\*.py -Destination $tmp_path\plugin -Force
 
 $zip_path = "$project_path\io_scene_md5model.zip"
